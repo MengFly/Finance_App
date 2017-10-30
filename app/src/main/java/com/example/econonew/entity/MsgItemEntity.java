20a25afa76f6dfcd4ef632adc3ed87a0f7f927a3
mengfly <www.wangpengfei99@gmail.com>
2017-10-30 22:33:32 +0800

    :100:

--- app/src/main/java/com/example/econonew/entity/MsgItemEntity.java
+++ app/src/main/java/com/example/econonew/entity/MsgItemEntity.java
@@ -13,157 +13,173 @@ import java.io.Serializable;
  * 这个类是用来管理从服务器哪里接收到的消息 将从服务器哪里接收到的消息内容存储在这个类中
  *
  * @author mengfei
- *
  */
-public class MsgItemEntity extends DataSupport implements Serializable, Saveable {
+public class MsgItemEntity extends DataSupport implements Serializable, Saveable, Comparable<MsgItemEntity> {
 
-	private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
 
-	public static final String ADDRESS_SERVER = "server";//中间服务器
-	public static final String ADDRESS_SERVER_BACK = "server_back";//采集服务器
+    public static final String ADDRESS_SERVER = "server";//中间服务器
+    public static final String ADDRESS_SERVER_BACK = "server_back";//采集服务器
 
-	private String msgType;
+    private String msgType;
+    //消息的时间信息
+    public int msgVerifierTime;
 
-	private int id;
+    private int id;
 
-	public int getId() {
-		return id;
-	}
+    public int getId() {
+        return id;
+    }
 
-	public void setId(int id) {
-		this.id = id;
-	}
+    public void setId(int id) {
+        this.id = id;
+    }
 
-	public String getMsgType() {
-		return msgType;
-	}
+    public String getMsgType() {
+        return msgType;
+    }
 
-	public void setMsgType(String msgType) {
-		this.msgType = msgType;
-	}
+    public void setMsgType(String msgType) {
+        this.msgType = msgType;
+    }
 
-	@Column(unique = true)
-	// 存储消息标题
-	private String msgTitle;
+    public void setMsgVerifierTime(int msgVerifierTime) {
+        this.msgVerifierTime = msgVerifierTime;
+    }
 
-	// 存储消息内容
-	private String msgContent;
+    @Column(unique = true)
+    // 存储消息标题
+    private String msgTitle;
 
-	@Column(unique = true)
-	private String msgContentUrl;
-
-	// 存储消息摘要内容，主要用于朗读使用
-	private String msgGeneral;
-
-	// 存储消息的图片，即在ListView里面显示的图片
-	private String imageTitleUrl;
-
-	private boolean isLove;// 是否收藏
-
-	private int businessDomainId;
-
-	private int businessTypeId;
-
-	private int stairId;
-
-	public void setBusinessDomainId(int businessDomainId) {
-		this.businessDomainId = businessDomainId;
-	}
-
-	public void setBusinessTypeId(int businessTypeId) {
-		this.businessTypeId = businessTypeId;
-	}
-
-	public void setStairId(int stairId) {
-		this.stairId = stairId;
-	}
-
-	public int getBusinessDomainId() {
-		return businessDomainId;
-	}
-
-	public int getBusinessTypeId() {
-		return businessTypeId;
-	}
-
-	public int getStairId() {
-		return stairId;
-	}
+    // 存储消息内容
+    private String msgContent;
 
 
-	/**
-	 * @param msgTitle
-	 *            消息标题
-	 * @param msgContent
-	 *            消息内容-主要是用于展示在webView里面
-	 * @param msgGeneral
-	 *            消息摘要--主要是用在朗读部分
-	 * @param msgContentUrl
-	 *            消息内容目标网页的url
-	 * @param imageTitleUrl
-	 *            消息列表的图片的Url地址
-	 */
-	public 	MsgItemEntity(String msgTitle, String msgContent, String msgContentUrl, String msgGeneral,
-						 String imageTitleUrl) {
-		this.msgTitle = msgTitle;
-		this.msgContent = msgContent;
-		this.msgContentUrl = msgContentUrl;
-		this.msgGeneral = msgGeneral;
-		this.imageTitleUrl = imageTitleUrl;
-	}
+    @Column(unique = true)
+    private String msgContentUrl;
 
-	public String getMsgTitle() {
-		return msgTitle;
-	}
+    // 存储消息摘要内容，主要用于朗读使用
+    private String msgGeneral;
 
-	public String getMsgContent() {
-		return msgContent;
-	}
+    // 存储消息的图片，即在ListView里面显示的图片
+    private String imageTitleUrl;
 
-	public String getImageTitleUrl() {
-		return URLManager.getCaiBianURL() + imageTitleUrl;
-	}
+    private boolean isLove;// 是否收藏
 
-	public String getMsgGeneral() {
-		return msgGeneral;
-	}
+    private int businessDomainId;
 
-	public String getMsgContentUrl() {
-		return msgContentUrl;
-	}
+    private int businessTypeId;
 
-	public boolean isLove() {
-		return isLove;
-	}
+    private int stairId;
 
-	public void setLove(boolean isLove) {
-		this.isLove = isLove;
-	}
+    public void setBusinessDomainId(int businessDomainId) {
+        this.businessDomainId = businessDomainId;
+    }
 
-	private boolean isVip = false; // 是否是VIP信息
+    public void setBusinessTypeId(int businessTypeId) {
+        this.businessTypeId = businessTypeId;
+    }
 
-	public boolean isVip() {
-		return isVip;
-	}
+    public void setStairId(int stairId) {
+        this.stairId = stairId;
+    }
 
-	public void setVip(boolean isVip) {
-		this.isVip = isVip;
-	}
+    public int getBusinessDomainId() {
+        return businessDomainId;
+    }
+
+    public int getBusinessTypeId() {
+        return businessTypeId;
+    }
+
+    public int getStairId() {
+        return stairId;
+    }
 
 
-	/**
-	 * 判断此消息是否是这个频道的定制消息
-	 * @param entity 要判断的频道
-	 * @return 返回这条消息是否是这个频道的定制消息
+    /**
+     * @param msgTitle      消息标题
+     * @param msgContent    消息内容-主要是用于展示在webView里面
+     * @param msgGeneral    消息摘要--主要是用在朗读部分
+     * @param msgContentUrl 消息内容目标网页的url
+     * @param imageTitleUrl 消息列表的图片的Url地址
      */
-	public boolean isDingZhi(ChannelEntity entity) {
-		return this.getBusinessDomainId() == entity.getBusinessDomainId() &&
-				this.getBusinessTypeId() == entity.getBusinessTypeId() &&
-				this.getStairId() == entity.getStairId();
-	}
+    public MsgItemEntity(String msgTitle, String msgContent, String msgContentUrl, String msgGeneral,
+                         String imageTitleUrl) {
+        this.msgTitle = msgTitle;
+        this.msgContent = msgContent;
+        this.msgContentUrl = msgContentUrl;
+        this.msgGeneral = msgGeneral;
+        this.imageTitleUrl = imageTitleUrl;
+    }
 
-	@Override
-	public boolean saveSelf() {
-		return save();
-	}
+    public String getMsgTitle() {
+        return msgTitle;
+    }
+
+    public String getMsgContent() {
+        return msgContent;
+    }
+
+    public String getImageTitleUrl() {
+        return URLManager.getCaiBianURL() + imageTitleUrl;
+    }
+
+    public String getMsgGeneral() {
+        return msgGeneral;
+    }
+
+    public String getMsgContentUrl() {
+        return msgContentUrl;
+    }
+
+    public boolean isLove() {
+        return isLove;
+    }
+
+    public void setLove(boolean isLove) {
+        this.isLove = isLove;
+    }
+
+    private boolean isVip = false; // 是否是VIP信息
+
+    public boolean isVip() {
+        return isVip;
+    }
+
+    public void setVip(boolean isVip) {
+        this.isVip = isVip;
+    }
+
+
+    /**
+     * 判断此消息是否是这个频道的定制消息
+     *
+     * @param entity 要判断的频道
+     * @return 返回这条消息是否是这个频道的定制消息
+     */
+    public boolean isDingZhi(ChannelEntity entity) {
+        return this.getBusinessDomainId() == entity.getBusinessDomainId() &&
+                this.getBusinessTypeId() == entity.getBusinessTypeId() &&
+                this.getStairId() == entity.getStairId();
+    }
+
+    public int compareTo(MsgItemEntity o) {              //利用时间戳实现对象间的比较
+        if (msgVerifierTime > o.msgVerifierTime) {
+            return 1;
+        } else if (msgVerifierTime < o.msgVerifierTime) {
+            return -1;
+        }
+        return 0;
+    }
+
+    public String toString() {
+        return String.valueOf(msgVerifierTime);
+    }
+
+    @Override
+    public boolean saveSelf() {
+        return save();
+    }
+
 }
